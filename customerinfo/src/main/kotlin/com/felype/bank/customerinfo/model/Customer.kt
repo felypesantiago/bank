c4f5be570fecdc6dbf6a3bba65da632eded00a96
Felype Santiago Ferreira <felype.ferreira@gmail.com>
2020-04-17 12:20:10 +0300

    Added Swagger documentation.

--- customerinfo/src/main/kotlin/com/felype/bank/customerinfo/model/Customer.kt
+++ customerinfo/src/main/kotlin/com/felype/bank/customerinfo/model/Customer.kt
@@ -2,11 +2,38 @@ package com.felype.bank.customerinfo.model
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import java.time.LocalDate
 import javax.persistence.*
 
 @Entity
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 @Table(name = "customers")
-data class Customer(@GeneratedValue(strategy = GenerationType.IDENTITY) @Id val id: Long,
-                    val firstName: String, val lastName: String, val ssn: String, val birthDate: LocalDate)
+@ApiModel(description = "Represents a bank customer.")
+data class Customer(@GeneratedValue(strategy = GenerationType.IDENTITY)
+                    @Id
+                    @ApiModelProperty(required = true,
+                            value = "Unique Customer ID.",
+                            example = "1")
+                    val id: Long,
+
+                    @ApiModelProperty(required = true,
+                            value = "Customer's first name.",
+                            example = "Fulano")
+                    val firstName: String,
+
+                    @ApiModelProperty(required = true,
+                            value = "Customer's last name.",
+                            example = "de Tal")
+                    val lastName: String,
+
+                    @ApiModelProperty(required = true,
+                            value = "Customer's Social Security Number.",
+                            example = "201087-2020")
+                    val ssn: String,
+
+                    @ApiModelProperty(required = true,
+                            value = "Customer's birth date.",
+                            example = "1987-10-20")
+                    val birthDate: LocalDate)
--- customerinfo/src/main/kotlin/com/felype/bank/customerinfo/resource/CustomerInfoResource.kt
+++ customerinfo/src/main/kotlin/com/felype/bank/customerinfo/resource/CustomerInfoResource.kt
@@ -2,17 +2,31 @@ package com.felype.bank.customerinfo.resource
 
 import com.felype.bank.customerinfo.model.Customer
 import com.felype.bank.customerinfo.service.CustomerService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import java.net.HttpURLConnection
 
+@Api
 @RestController
 @RequestMapping("/customers", produces = [MediaType.APPLICATION_JSON_VALUE])
 class CustomerInfoResource(private val customerService: CustomerService) {
 
+    @ApiOperation(value = "Get customer by ID.",
+            notes = "Allows client to retrieve customer Data.",
+            tags = ["customer"])
+    @ApiResponses(
+            ApiResponse(code = HttpURLConnection.HTTP_OK, message = "Customer successfully retrieved.", response = Customer::class),
+            ApiResponse(code = HttpURLConnection.HTTP_NOT_FOUND, message = "Customer with the given ID not found."),
+            ApiResponse(code = HttpURLConnection.HTTP_INTERNAL_ERROR, message = "Internal server error."),
+            ApiResponse(code = HttpURLConnection.HTTP_UNAVAILABLE, message = "Service temporarily unavailable due to service maintenance, heavy load or other issues."))
     @GetMapping("/{customerId}")
     fun getCustomer(@PathVariable("customerId") id: Long): Mono<Customer> {
         return customerService.getCustomer(id)
